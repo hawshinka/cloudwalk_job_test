198fa48958c0a539b63ed2f3041be7bc99e5eab6
Yauari Vieira <yauari.vieira@traivefinance.com>
2024-01-13 20:07:51 -0300

    rearranging parser package and creating a simple Makefile to run the application

--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+
+	"qgames/parser"
 )
 
 const filename = "qgames.log"
 
 func main() {
-	p := Parser{}
+	p := parser.Parser{}
 	err := p.Parse(filename)
 	if err != nil {
 		panic(err)
