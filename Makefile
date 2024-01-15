1cb4e6f5b23efb2b49a9a152398b7fbfa815f066
Yauari Vieira <yauari.vieira@traivefinance.com>
2024-01-15 15:34:40 -0300

    improving main file alongside with the makefile, accepting (or not) in and out variables referencing the files

--- Makefile
+++ Makefile
@@ -1,2 +1,22 @@
-run:
-	go run main.go
+in := qgames.log
+out := qgames.json
+
+unit-test:
+	# Running unit tests...
+	go test -race -tags=unit ./...
+
+unit-test-coverage:
+	# Generating coverage from tests...
+	go test -race -tags=unit ./... -v -coverprofile coverage.txt
+	go tool cover -html=coverage.txt -o coverage.html
+	rm coverage.txt
+
+validate-in-file:
+	@ if [ -z "${in}" ]; then echo "Error: 'in' variable is not set. Defaulting to qgames.log."; fi
+
+validate-out-file:
+	@ if [ -z "${out}" ]; then echo "Error: 'out' variable is not set. Defaulting to qgames.json."; fi
+
+run: validate-in-file validate-out-file
+	# Running application...
+	go run main.go -in=${in} -out=${out}
--- main.go
+++ main.go
@@ -1,21 +1,48 @@
 package main
 
 import (
-	"encoding/json"
+	"flag"
 	"fmt"
+	"io"
+	"os"
 
 	"qgames/parser"
 )
 
-const filename = "qgames.log"
-
 func main() {
+	// Define flags
+	var inFile string
+	var outFile string
+
+	// Parse command-line arguments
+	flag.StringVar(&inFile, "in", "qgames.log", "Input file name")
+	flag.StringVar(&outFile, "out", "qgames.json", "Output file name")
+	flag.Parse()
+
 	p := parser.Parser{}
-	err := p.Parse(filename)
+	parsedLog, err := p.Parse(inFile)
 	if err != nil {
 		panic(err)
 	}
+	fmt.Println(parsedLog)
+	if err := writeOutputToFile(outFile, parsedLog); err != nil {
+		fmt.Println(parsedLog)
+		panic(err)
+	}
 
-	out, _ := json.Marshal(p.Log)
-	fmt.Println(string(out))
+}
+
+func writeOutputToFile(outFile, parsedLog string) error {
+	f, err := os.Create(outFile)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	_, err = io.WriteString(f, parsedLog)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
